cc7e0851dafbbfd68ad151493aaab4b5ae141a7f
Vadim <matokvadym@gmail.com>
2023-09-05 20:27:28 +0300

    Commit AboutUsPage(Not Full)

---
+++ Matoc_Cars/Controllers/AboutUsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Matoc_Cars.Controllers;
+
+public class AboutUsController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
